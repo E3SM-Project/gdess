d2a7a78a8f4f83208eb73ad2c99266878697d939
kauf418 <dkauf42@gmail.com>
2020-09-30 17:43:46 -0400

    add initial setup and requirements files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='e3sm_co2_diag',
+    version='0.0a1',
+    description='Package of tools for evaluating CO2 in E3SM',
+    author='Daniel E. Kaufman',
+    author_email='daniel.kaufman@pnnl.gov',
+    packages=find_packages(),  #['e3sm_co2_diag'],
+    )
