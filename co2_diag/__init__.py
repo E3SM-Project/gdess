91fa155a6ac09f16f085b116c990ff396a88fe40
kauf418 <dkauf42@gmail.com>
2020-10-12 16:23:10 -0400

    consolidate num&str formatting functions into a 'formatters' package with co2_diag

--- co2_diag/__init__.py
+++ co2_diag/__init__.py
@@ -1,20 +1,21 @@
-import os
-import logging.config
-import json
 
-# If applicable, delete the existing log file to generate a fresh log file during each execution
-# if os.path.isfile("python_logging.log"):
-#     os.remove("python_logging.log")
+def _config_logger():
+    """Configure the root logger"""
+    import os
+    import logging.config
+    import json
 
-# Config the root logger
-path = os.path.dirname(os.path.realpath(__file__)) + '/config/log_config.json'
-with open(path, 'r') as logging_configuration_file:
-    config_dict = json.load(logging_configuration_file)
+    logconfig_path = os.path.dirname(os.path.realpath(__file__)) + '/config/log_config.json'
+    with open(logconfig_path, 'r') as logging_configuration_file:
+        config_dict = json.load(logging_configuration_file)
 
-logging.config.dictConfig(config_dict)
+    logging.config.dictConfig(config_dict)
 
 
 def _change_log_level(a_logger, level):
     a_logger.setLevel(level)
     for handler in a_logger.handlers:
         handler.setLevel(level)
+
+
+_config_logger()
