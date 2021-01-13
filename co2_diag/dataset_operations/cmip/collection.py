79f5949df6563de7457e636c8d9f730f39792497
kauf418 <dkauf42@gmail.com>
2021-01-13 17:52:39 -0500

    move colormap method to multiset

--- co2_diag/dataset_operations/cmip/collection.py
+++ co2_diag/dataset_operations/cmip/collection.py
@@ -15,7 +15,6 @@ from co2_diag.graphics.utils import asthetic_grid_no_spines, mysavefig
 import intake
 import intake_esm
 
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 import logging
@@ -409,43 +408,6 @@ class Collection(Multiset):
                                                        coords_as_dimensions=True)
 
         return xr_ds.stack(coord_pair=['lat', 'lon']).isel(coord_pair=closest_point_dict['index'])
-
-    @staticmethod
-    def categorical_cmap(nc, nsc, cmap="tab10", continuous=False):
-        """
-        Parameters
-        ----------
-        nc
-            number of categories
-        nsc
-            number of subcategories
-        cmap
-        continuous
-
-        Returns
-        -------
-            A colormap with nc*nsc different colors, where for each category there are nsc colors of same hue
-
-        Notes
-        -----
-            from https://stackoverflow.com/questions/47222585/matplotlib-generic-colormap-from-tab10
-        """
-        if nc > plt.get_cmap(cmap).N:
-            raise ValueError("Too many categories for colormap.")
-        if continuous:
-            ccolors = plt.get_cmap(cmap)(np.linspace(0, 1, nc))
-        else:
-            ccolors = plt.get_cmap(cmap)(np.arange(nc, dtype=int))
-        cols = np.zeros((nc * nsc, 3))
-        for i, c in enumerate(ccolors):
-            chsv = mpl.colors.rgb_to_hsv(c[:3])
-            arhsv = np.tile(chsv, nsc).reshape(nsc, 3)
-            arhsv[:, 1] = np.linspace(chsv[1], 0.25, nsc)
-            arhsv[:, 2] = np.linspace(chsv[2], 1, nsc)
-            rgb = mpl.colors.hsv_to_rgb(arhsv)
-            cols[i * nsc:(i + 1) * nsc, :] = rgb
-        cmap = mpl.colors.ListedColormap(cols)
-        return cmap
 
     def plot_timeseries(self) -> (plt.Figure, plt.Axes, tuple):
         """Make timeseries plot of co2 concentrations from or more CMIP models
--- co2_diag/dataset_operations/multiset.py
+++ co2_diag/dataset_operations/multiset.py
@@ -3,6 +3,9 @@ import pickle
 from typing import Union
 import numpy as np
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
 import co2_diag.dataset_operations as co2ops
 from co2_diag.dataset_operations.datasetdict import DatasetDict
 
@@ -106,6 +109,43 @@ class Multiset:
         _df_anomaly_mean_cycle = df_anomaly.groupby('moy').mean().reset_index()
 
         return _df_anomaly_mean_cycle, _df_anomaly_yearly
+
+    @staticmethod
+    def categorical_cmap(nc, nsc, cmap="tab10", continuous=False):
+        """
+        Parameters
+        ----------
+        nc
+            number of categories
+        nsc
+            number of subcategories
+        cmap
+        continuous
+
+        Returns
+        -------
+            A colormap with nc*nsc different colors, where for each category there are nsc colors of same hue
+
+        Notes
+        -----
+            from https://stackoverflow.com/questions/47222585/matplotlib-generic-colormap-from-tab10
+        """
+        if nc > plt.get_cmap(cmap).N:
+            raise ValueError("Too many categories for colormap.")
+        if continuous:
+            ccolors = plt.get_cmap(cmap)(np.linspace(0, 1, nc))
+        else:
+            ccolors = plt.get_cmap(cmap)(np.arange(nc, dtype=int))
+        cols = np.zeros((nc * nsc, 3))
+        for i, c in enumerate(ccolors):
+            chsv = mpl.colors.rgb_to_hsv(c[:3])
+            arhsv = np.tile(chsv, nsc).reshape(nsc, 3)
+            arhsv[:, 1] = np.linspace(chsv[1], 0.25, nsc)
+            arhsv[:, 2] = np.linspace(chsv[2], 1, nsc)
+            rgb = mpl.colors.hsv_to_rgb(arhsv)
+            cols[i * nsc:(i + 1) * nsc, :] = rgb
+        cmap = mpl.colors.ListedColormap(cols)
+        return cmap
 
     def __repr__(self):
         """ String representation is built.
