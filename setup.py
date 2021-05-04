7059791f9cef3ed9535caa493228c2a88aa6fbcb
kauf418 <dkauf42@gmail.com>
2021-05-04 18:46:55 -0400

    move setup steps to use setup.cfg file

--- setup.py
+++ setup.py
@@ -1,11 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='e3sm_co2_diag',
-    version='0.0a1',
-    description='Package of tools for evaluating CO2 in E3SM',
-    author='Daniel E. Kaufman',
-    author_email='daniel.kaufman@pnnl.gov',
-    packages=find_packages(),  #['e3sm_co2_diag'],
-    include_package_data=True
-    )
+import setuptools
+setuptools.setup()
