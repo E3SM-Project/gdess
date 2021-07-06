1ca04977198de8472b8cacabe5197c48bda725fd
kauf418 <dkauf42@gmail.com>
2021-07-06 13:13:18 -0400

    add altitude to test_operations dataset fixture

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -11,27 +11,29 @@ import cftime, pytest, logging
 def random_data():
     # Create random data
     np.random.seed(0)
-    co2 = 10 * np.random.rand(2, 2, 3, 3)
-    lon = [[-99.83, -99.32], [-99.79, -99.23]]
-    lat = [[42.25, 42.21], [42.63, 42.59]]
+    co2 = 10 * np.random.rand(5, 5, 3, 4)
+    zg = [50, 75, 100] + (10 * np.random.rand(5, 5, 4, 3))
+    zg = np.swapaxes(zg, 2, 3)
+    lon = [-180, -90, 0, 90, 180]
+    lat = [-90, -45, 0, 45, 90]
     plev = [50, 75, 100]
-    time = pd.date_range("2014-09-06", periods=3)
+    time = pd.date_range("2014-09-06", periods=4)
     reference_time = pd.Timestamp("2014-09-05")
 
-    return co2, lon, lat, time, plev, reference_time
+    return co2, zg, lon, lat, time, plev, reference_time
 
 
 @pytest.fixture
 def dataarray_withco2(random_data):
-    co2, lon, lat, time, plev, reference_time = random_data
+    co2, _, lon, lat, time, plev, reference_time = random_data
 
     # Initialize a dataarray with multiple dimensions:
     da = xr.DataArray(
         data=co2,
-        dims=["x", "y", "plev", "time"],
+        dims=["lon", "lat", "plev", "time"],
         coords=dict(
-            lon=(["x", "y"], lon),
-            lat=(["x", "y"], lat),
+            lon=lon,
+            lat=lat,
             plev=plev,
             time=time,
             reference_time=reference_time,
@@ -45,17 +47,18 @@ def dataarray_withco2(random_data):
 
 
 @pytest.fixture
-def dataset_withco2(random_data):
-    co2, lon, lat, time, plev, reference_time = random_data
+def dataset_withco2andzg(random_data):
+    co2, zg, lon, lat, time, plev, reference_time = random_data
 
     # Initialize a dataset:
     ds = xr.Dataset(
         data_vars=dict(
-            co2=(["x", "y", "plev", "time"], co2),
+            co2=(["lon", "lat", "plev", "time"], co2),
+            zg=(["lon", "lat", "plev", "time"], zg),
         ),
         coords=dict(
-            lon=(["x", "y"], lon),
-            lat=(["x", "y"], lat),
+            lon=lon,
+            lat=lat,
             plev=plev,
             time=time,
             reference_time=reference_time,
@@ -86,19 +89,19 @@ def test_month_list_generation():
     assert monthlist(['2020-11-01', '2021-05-05']) == expected
 
 
-def test_unit_conversion_dataset(dataset_withco2):
-    an_original_value = dataset_withco2['co2'].isel(x=0, y=0, plev=0, time=0).values
+def test_unit_conversion_dataset(dataset_withco2andzg):
+    an_original_value = dataset_withco2andzg['co2'].isel(lon=0, lat=0, plev=0, time=0).values
 
-    newdataset = co2_kgfrac_to_ppm(dataset_withco2, co2_var_name='co2')
+    newdataset = co2_kgfrac_to_ppm(dataset_withco2andzg, co2_var_name='co2')
 
-    a_new_value = newdataset['co2'].isel(x=0, y=0, plev=0, time=0).values
+    a_new_value = newdataset['co2'].isel(lon=0, lat=0, plev=0, time=0).values
 
     assert a_new_value == (an_original_value * (28.9647 / 44.01) * 1e6)
 
 
-def test_unit_conversion_dataset_bad_varname_raises(dataset_withco2):
+def test_unit_conversion_dataset_bad_varname_raises(dataset_withco2andzg):
     with pytest.raises(Exception):
-        co2_kgfrac_to_ppm(dataset_withco2, co2_var_name='carbon dio')
+        co2_kgfrac_to_ppm(dataset_withco2andzg, co2_var_name='carbon dio')
 
 
 def test_conversion_of_datetime_to_a_decimalyear_time():
