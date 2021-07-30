01ba4b02ce46519b74e7f8973dd04180f7d54886
kauf418 <dkauf42@gmail.com>
2021-07-30 13:43:07 -0400

    modify default start and end years for recipes

--- co2_diag/recipes/utils.py
+++ co2_diag/recipes/utils.py
@@ -16,8 +16,10 @@ def add_shared_arguments_for_recipes(parser: argparse.ArgumentParser) -> None:
 
     parser.add_argument('ref_data', nargs='?', default=None, type=valid_existing_path,
                         help='Filepath to the reference data folder')
-    parser.add_argument('--start_yr', default="1960", type=valid_year_string, help='Initial year cutoff')
-    parser.add_argument('--end_yr', default="2015", type=valid_year_string, help='Final year cutoff')
+    parser.add_argument('--start_yr', default="1958", type=valid_year_string,
+                        help='Initial year cutoff. Default is 1958, which is the first year of the Mauna Loa CO2 record.')
+    parser.add_argument('--end_yr', default="2014", type=valid_year_string,
+                        help='Final year cutoff. Default is 2014, which is the final year for CMIP6 historical runs.')
     parser.add_argument('--figure_savepath', type=str, default=None, help='Filepath for saving generated figures')
 
 
