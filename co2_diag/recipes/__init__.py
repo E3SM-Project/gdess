37d4e4dab060ab00eaf1a109d0ba6d9831497e16
kauf418 <dkauf42@gmail.com>
2021-08-24 11:12:45 -0400

    update heatmap figure layout for seasonal cycles recipe

--- co2_diag/recipes/__init__.py
+++ co2_diag/recipes/__init__.py
@@ -1,2 +1,3 @@
 from .surface_trends import surface_trends
 from .seasonal_cycles import seasonal_cycles
+from .meridional_gradient import meridional_gradient
--- co2_diag/recipes/seasonal_cycles.py
+++ co2_diag/recipes/seasonal_cycles.py
@@ -428,7 +428,8 @@ def plot_heatmap_of_all_stations(xdata: pd.DataFrame,
 
     # --- Plot the seasonal cycle for all stations,
     #   and flip the ydata because pyplot.imshow will plot the last row on the bottom
-    fig, ax = plt.subplots(1, 1, figsize=(6, num_stations*0.8))
+    totalfigheight = (num_stations*0.8) + (num_stations*0.8)*0.15  # Add 0.15 for the colorbar
+    fig, ax = plt.subplots(1, 1, figsize=(6, totalfigheight))
     im = ax.imshow(ydata.transpose().iloc[::-1],
                    norm=mcolors.TwoSlopeNorm(vcenter=0.), cmap='RdBu_r', interpolation='nearest',
                    aspect='auto', extent=(mindate, maxdate, -0.5, num_stations - 0.5))
@@ -446,8 +447,9 @@ def plot_heatmap_of_all_stations(xdata: pd.DataFrame,
         ax2.set_yticks(range(num_stations))
         ax2.set_yticklabels(rightside_labels)
     #
-    cbar = fig.colorbar(im, orientation="horizontal", pad=max([0.5, (num_stations**0.5)*0.09]))
+    cbar = fig.colorbar(im, orientation="horizontal", pad=max([0.5, (num_stations - 0.5)*0.008]))
     cbar.ax.set_xlabel('$CO_2$ (ppm)')
+    cbar.ax.set_xticklabels(cbar.ax.get_xticklabels(), rotation='vertical')
     #
     # Specify the xaxis tick labels format -- %b gives us Jan, Feb...
     month_fmt = mdates.DateFormatter('%b')
