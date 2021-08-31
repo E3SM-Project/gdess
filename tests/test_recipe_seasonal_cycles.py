ff602cc0117ec81d6773ac6f7a214e64226fc9cb
kauf418 <dkauf42@gmail.com>
2021-08-31 00:23:03 -0400

    update recipe arguments in tests

--- tests/test_recipe_seasonal_cycles.py
+++ tests/test_recipe_seasonal_cycles.py
@@ -13,10 +13,10 @@ def test_recipe_input_year_error(globalview_test_data_path):
     recipe_options = {
         'ref_data': globalview_test_data_path,
         'model_name': 'BCC.esm-hist',
-        'station_code': 'mlo',
         'start_yr': "198012",
         'end_yr': "201042",
-        'figure_savepath': 'test_figure'}
+        'figure_savepath': './outputs',
+        'station_list': 'mlo'}
     with pytest.raises(SystemExit):
         seasonal_cycles(verbose='DEBUG', options=recipe_options)
 
@@ -25,10 +25,10 @@ def test_recipe_input_model_error(globalview_test_data_path):
     recipe_options = {
         'ref_data': globalview_test_data_path,
         'model_name': 'BCasdasdjkhgC',
-        'station_code': 'mlo',
         'start_yr': "1980",
         'end_yr': "2010",
-        'figure_savepath': 'test_figure'}
+        'figure_savepath': './outputs',
+        'station_list': 'mlo'}
     with pytest.raises(SystemExit):
         seasonal_cycles(verbose='DEBUG', options=recipe_options)
 
@@ -37,10 +37,10 @@ def test_recipe_input_stationcode_error(globalview_test_data_path):
     recipe_options = {
         'ref_data': globalview_test_data_path,
         'model_name': 'BCC.esm-hist',
-        'station_code': 'asdkjhfasg',
         'start_yr': "1980",
         'end_yr': "2010",
-        'figure_savepath': 'test_figure'}
+        'figure_savepath': './outputs',
+        'station_list': 'asdkjhfasg'}
     with pytest.raises(SystemExit):
         seasonal_cycles(verbose='DEBUG', options=recipe_options)
 
@@ -49,10 +49,10 @@ def test_recipe_completes_with_no_errors(globalview_test_data_path):
     recipe_options = {
         'ref_data': globalview_test_data_path,
         'model_name': 'BCC.esm-hist',
-        'station_code': 'mlo',
         'start_yr': "1980",
         'end_yr': "2010",
-        'figure_savepath': 'test_figure'}
+        'figure_savepath': './outputs',
+        'station_list': 'mlo'}
     try:
         data_dict = seasonal_cycles(verbose='DEBUG', options=recipe_options)
     except Exception as exc:
@@ -65,8 +65,8 @@ def test_recipe_bin_multiple_stations_completes_with_no_errors(globalview_test_d
         'latitude_bin_size': 30,
         'start_yr': "1980",
         'end_yr': "2010",
-        'figure_savepath': 'test_figure',
-        'station_list': 'mlo'}
+        'figure_savepath': './outputs',
+        'station_list': 'mlo smo'}
     try:
         data_dict = seasonal_cycles(verbose='DEBUG', options=recipe_options)
     except Exception as exc:
