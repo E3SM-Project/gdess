765405a5d161de56f7648a7946145c4f9ff7d054
kauf418 <dkauf42@gmail.com>
2021-09-02 12:59:56 -0400

    add info message to path setting in tests dir

--- tests/set_path_vars_for_testing.sh
+++ tests/set_path_vars_for_testing.sh
@@ -9,6 +9,7 @@
 # Creation date: August 2021
 # ------------------------------------------------------------------
 path_to_repo=${GDESS_REPO}  # For example: ${HOME}/gdess
+echo "Path to repo has been set to <${path_to_repo}>"
 
 export GDESS_CMIP_DATA=${path_to_repo}/tests/test_data/cmip/
 export GDESS_GLOBALVIEW_DATA=${path_to_repo}/tests/test_data/globalview/
