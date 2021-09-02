ac2719188086ff1a9834da787eefce21afa40db5
kauf418 <dkauf42@gmail.com>
2021-09-02 13:48:34 -0400

    add test module for meridional gradient recipe

---
+++ tests/test_recipe_meridional.py
@@ -0,0 +1,73 @@
+import os
+import pytest
+
+from co2_diag.recipes import meridional_gradient
+
+
+@pytest.fixture
+def globalview_test_data_path(rootdir):
+    return os.path.join(rootdir, 'test_data', 'globalview')
+
+
+def test_recipe_input_year_error(globalview_test_data_path):
+    recipe_options = {
+        'ref_data': globalview_test_data_path,
+        'model_name': 'BCC.esm-hist',
+        'start_yr': "198012",
+        'end_yr': "201042",
+        'figure_savepath': './outputs',
+        'station_list': 'mlo'}
+    with pytest.raises(SystemExit):
+        meridional_gradient(verbose='DEBUG', options=recipe_options)
+
+
+def test_recipe_input_model_error(globalview_test_data_path):
+    recipe_options = {
+        'ref_data': globalview_test_data_path,
+        'model_name': 'BCasdasdjkhgC',
+        'start_yr': "1980",
+        'end_yr': "2010",
+        'figure_savepath': './outputs',
+        'station_list': 'mlo'}
+    with pytest.raises(SystemExit):
+        meridional_gradient(verbose='DEBUG', options=recipe_options)
+
+
+def test_recipe_input_stationcode_error(globalview_test_data_path):
+    recipe_options = {
+        'ref_data': globalview_test_data_path,
+        'model_name': 'BCC.esm-hist',
+        'start_yr': "1980",
+        'end_yr': "2010",
+        'figure_savepath': './outputs',
+        'station_list': 'asdkjhfasg'}
+    with pytest.raises(SystemExit):
+        meridional_gradient(verbose='DEBUG', options=recipe_options)
+
+
+def test_recipe_completes_with_no_errors(globalview_test_data_path):
+    recipe_options = {
+        'ref_data': globalview_test_data_path,
+        'model_name': 'BCC.esm-hist',
+        'start_yr': "1980",
+        'end_yr': "2010",
+        'figure_savepath': './outputs',
+        'station_list': 'mlo'}
+    try:
+        data_dict = meridional_gradient(verbose='DEBUG', options=recipe_options)
+    except Exception as exc:
+        assert False, f"'seasonal_cycles' raised an exception {exc}"
+
+
+def test_recipe_bin_multiple_stations_completes_with_no_errors(globalview_test_data_path):
+    recipe_options = {
+        'ref_data': globalview_test_data_path,
+        'latitude_bin_size': 30,
+        'start_yr': "1980",
+        'end_yr': "2010",
+        'figure_savepath': './outputs',
+        'station_list': 'mlo smo'}
+    try:
+        data_dict = meridional_gradient(verbose='DEBUG', options=recipe_options)
+    except Exception as exc:
+        assert False, f"'seasonal_cycles' raised an exception {exc}"
