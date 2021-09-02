bbc06a95372658f811d92443e2da4dc35c0e875d
kauf418 <dkauf42@gmail.com>
2021-09-02 12:49:35 -0400

    set up paths when initializing tests

---
+++ tests/__init__.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+import subprocess
+
+test_dir = os.path.dirname(os.path.abspath(__file__))
+pathing_script = os.path.join(test_dir, 'set_path_vars_for_testing.sh')
+
+# Set env variable to be visible in this process + all children
+os.environ['GDESS_REPO'] = str(Path(test_dir).parent.absolute())
+
+subprocess.call(['sh', f"./{pathing_script}"])
