daad3105f8248775a3db379a0c50376ec5b4e5cf
kauf418 <dkauf42@gmail.com>
2021-09-02 13:25:08 -0400

    move pathing for test setup to github actions env

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,5 @@
 import os
 import pytest
-from pathlib import Path
-import subprocess
-
-test_dir = os.path.dirname(os.path.abspath(__file__))
-pathing_script = os.path.join(test_dir, 'set_path_vars_for_testing.sh')
-
-# Set env variable to be visible in this process + all children
-os.environ['GDESS_REPO'] = str(Path(test_dir).parent.absolute())
-
-subprocess.call(['sh', f"./{pathing_script}"])
 
 
 @pytest.fixture
